6fef879282f8baa246a7d13489a86667832ac041
choudhary9971 <shoaibahmad269@gmail.com>
2024-01-15 05:17:28 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAuTc9ziIMVtdSv5Cw_Q_Y5amXJLopdE54",
+  authDomain: "new-project-d3019.firebaseapp.com",
+  projectId: "new-project-d3019",
+  storageBucket: "new-project-d3019.appspot.com",
+  messagingSenderId: "66585072899",
+  appId: "1:66585072899:web:f2f9cbd89b6c8759790536"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
